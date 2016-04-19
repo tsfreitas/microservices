041aa16b1a250bc517e2167a7bf476ed342c0c0f
THIAGO SANTOS DE FREITAS <thiago.freitas@buscapecompany.com>
2016-04-19 18:44:10 -0300

    um monte de configuração

--- api-gateway/src/main/java/com/tsfreitas/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/tsfreitas/ApiGatewayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
