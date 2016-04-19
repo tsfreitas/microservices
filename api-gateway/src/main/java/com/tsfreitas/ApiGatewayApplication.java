cd7cb579d3292dbb3a88fa0360a1f0d4befa0654
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-04-19 08:12:50 -0300

    criação do projeto api-gateway

---
+++ api-gateway/src/main/java/com/tsfreitas/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.tsfreitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
